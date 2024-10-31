a0ba59831c12c9d19659ad34c0a9094a0d9dd48f
Jahnavi Maddhuri <jahnavi.maddhuri@gmail.com>
2024-10-30 22:23:51 -0400

    change path of test

--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,6 @@
 from main import calculate_mean
 
 def test_calculate_mean():
-    mean = calculate_mean()
+    mean = calculate_mean('src/data/bad-drivers.csv')
     assert int(mean) == 134
     return
